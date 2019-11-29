02b8cc1a17cad940b690e82aaab3e7923ba01172
Marlon Paulo <marlon.paulo.silva@outlook.com>
2019-11-29 12:38:03 -0300

    criando o dashboard profile controller

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -4,12 +4,13 @@ module.exports = {
   async store(req, res) {
     const { name, email, city } = req.body;
 
-    let user = await User.findOne({ name, email, city });
+    let userf = await User.findOne({ email });
 
-    if (!user) {
+    if (!userf) {
       const user = await User.create({ name, email, city });
+      return res.json(user);
     }
 
-    return res.json(user);
+    return res.json(userf);
   }
 };
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,13 +4,17 @@ const uploadConfig = require("./config/upload");
 
 const SessionController = require("./controllers/SessionController");
 const OfferController = require("./controllers/OfferController");
+const DashboardProfileController = require("./controllers/DashboardProfileController");
 
 const routes = express.Router();
 
 const upload = multer(uploadConfig);
 
 routes.post("/sessions", SessionController.store);
+
 routes.get("/offers", OfferController.index);
 routes.post("/offers", upload.single("thumbnail"), OfferController.store);
+
+routes.get("/dashboardprofile", DashboardProfileController.show);
 
 module.exports = routes;

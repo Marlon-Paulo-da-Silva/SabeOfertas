e176a63134dd106d350a6ee0554c0c435d1e3f4c
Marlon Paulo <marlon.paulo.silva@outlook.com>
2019-12-01 23:55:07 -0300

    model store criado

--- backend/src/models/Store.js
+++ backend/src/models/Store.js
@@ -1,1 +1,10 @@
 const mongoose = require("mongoose");
+
+const StoreSchema = mongoose.Schema({
+  companyPicture: String,
+  local: String,
+  company: String,
+  description: String
+});
+
+module.exports = mongoose.model("Store", StoreSchema);

d54572d999e98fee48f62521e79d6ccbac9c4219
Marlon Paulo <marlon.paulo.silva@outlook.com>
2019-12-04 22:13:14 -0300

    button axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
-import api from "./services/api";
 import "./App.css";
-
-import logo from "./assets/logobo.svg";
 import PlacesAutocomplete, {
   geocodeByAddress,
   getLatLng
 } from "react-places-autocomplete";
+
+import logo from "./assets/logosabeoferta.svg";
 
 function App() {
   const [address, setAddress] = React.useState("");
@@ -20,13 +19,7 @@ function App() {
     const latLng = await getLatLng(results[0]);
     setCoordinates(latLng);
     await setAddress(value);
-    return alert("Voce esta pesquisando promoções em " + address);
   };
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    console.log("Handle submit funcionando!!");
-  }
 
   return (
     <div className="container">
@@ -37,7 +30,7 @@ function App() {
           ofertas
         </p>
 
-        <form onSubmit={handleSubmit}>
+        <form>
           <PlacesAutocomplete
             value={address}
             onChange={setAddress}
@@ -52,7 +45,7 @@ function App() {
               <div>
                 <input
                   {...getInputProps({
-                    placeholder: "Digite o endereço da sua localização",
+                    placeholder: "Digite o cidade da sua localização",
                     className: "location-search-input"
                   })}
                 />
@@ -78,6 +71,9 @@ function App() {
               </div>
             )}
           </PlacesAutocomplete>
+          <button className="btn" type="submit">
+            Pesquisar
+          </button>
         </form>
       </div>
     </div>

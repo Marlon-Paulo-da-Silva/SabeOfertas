8fa8acef76820a2485de2daebad04971ea7727ca
Marlon Paulo <marlon.paulo.silva@outlook.com>
2019-12-05 03:26:39 -0300

    limpando app.js e adicionando na pagina Search

--- frontend/src/pages/Search/index.js
+++ frontend/src/pages/Search/index.js
@@ -1,5 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function Search() {
-  return <div />;
+import api from "../../services/api";
+
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng
+} from "react-places-autocomplete";
+
+import logo from "../../assets/logosabeoferta.svg";
+
+export default function Search({ history }) {
+  const [address, setAddress] = React.useState("");
+  const [coordinates, setCoordinates] = useState({
+    lat: null,
+    lng: null
+  });
+
+  const handleSelect = async value => {
+    const results = await geocodeByAddress(value);
+    const latLng = await getLatLng(results[0]);
+    setCoordinates(latLng);
+    setAddress(value);
+  };
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const response = await api.get(`/offers/?city=${address}`);
+
+    console.log(response.data);
+
+    history.push("/home");
+  }
+
+  return (
+    <>
+      <img src={logo} alt="BuscaOferta" />
+      <p>
+        Escolha sua <strong>cidade</strong> para que possamos pesquisar ofertas
+      </p>
+
+      <form onSubmit={handleSubmit}>
+        <PlacesAutocomplete
+          value={address}
+          onChange={setAddress}
+          onSelect={handleSelect}
+        >
+          {({
+            getInputProps,
+            suggestions,
+            getSuggestionItemProps,
+            loading
+          }) => (
+            <div>
+              <input
+                {...getInputProps({
+                  placeholder: "Digite a cidade da sua localização",
+                  className: "location-search-input"
+                })}
+              />
+              <div>
+                {loading ? <div>...loading</div> : null}
+                {suggestions.map(suggestion => {
+                  const style = {
+                    backgroundColor: suggestion.active ? "#59125941" : "#fff"
+                  };
+
+                  return (
+                    <div
+                      {...getSuggestionItemProps(suggestion, {
+                        style,
+                        className: "inputSuggestion"
+                      })}
+                    >
+                      {suggestion.description}
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          )}
+        </PlacesAutocomplete>
+        <button className="btn" type="submit">
+          Pesquisar
+        </button>
+      </form>
+    </>
+  );
 }
